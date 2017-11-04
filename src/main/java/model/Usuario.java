2f52e8a01129a9ca075a336ce021336dd5812d41
Percy Maldonado Quispe <percy.maldonado@ucsp.edu.pe>
2017-11-04 18:04:49 -0500

    get-set

--- src/main/java/model/Usuario.java
+++ src/main/java/model/Usuario.java
@@ -22,4 +22,44 @@ public abstract class Usuario {
 	
 	@Column(length = 64, nullable = false)
 	private String Correo;
+
+	public Integer getId_usuario() {
+		return Id_usuario;
+	}
+
+	public void setId_usuario(Integer id_usuario) {
+		Id_usuario = id_usuario;
+	}
+
+	public String getNombre() {
+		return Nombre;
+	}
+
+	public void setNombre(String nombre) {
+		Nombre = nombre;
+	}
+
+	public String getApellidoPaterno() {
+		return ApellidoPaterno;
+	}
+
+	public void setApellidoPaterno(String apellidoPaterno) {
+		ApellidoPaterno = apellidoPaterno;
+	}
+
+	public String getApellidoMaterno() {
+		return ApellidoMaterno;
+	}
+
+	public void setApellidoMaterno(String apellidoMaterno) {
+		ApellidoMaterno = apellidoMaterno;
+	}
+
+	public String getCorreo() {
+		return Correo;
+	}
+
+	public void setCorreo(String correo) {
+		Correo = correo;
+	}
 }
