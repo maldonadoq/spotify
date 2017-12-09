58e4a27e6cfcb4d1418ea45963d588360a2275f0
Alonso Cerpa <alonso.cerpa@ucsp.edu.pe>
2017-12-09 08:20:29 -0500

    act

--- src/main/java/spotify/repository/UsuarioRepository.java
+++
@@ -1,9 +0,0 @@
-package spotify.repository;
-
-import java.util.List;
-import org.springframework.data.repository.CrudRepository;
-import spotify.model.UsuarioNormal;
-
-public interface UsuarioRepository extends CrudRepository<UsuarioNormal, String>{
-	List<UsuarioNormal> findAll();
-}
